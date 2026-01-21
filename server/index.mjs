876c3e3f2af15d5e4ffe5cbff7bf362a435b02da
sizan14789 <sizanalt@gmail.com>
2026-01-22 00:10:04 +0600

    supabase monitor added to keep the database online

--- server/index.mjs
+++ server/index.mjs
@@ -3,24 +3,24 @@ import express from "express";
 import connectDB from "./utils/connectDB.mjs";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import { createServer } from 'http'
+import { createServer } from "http";
 
 // Routers
-import authRouter from './routes/auth.mjs'
-import chatsRouter from './routes/chatsRouter.mjs'
-import messagesRouter from './routes/messagesRouter.mjs'
-import userRoutes from './routes/userRoutes.mjs'
+import authRouter from "./routes/auth.mjs";
+import chatsRouter from "./routes/chatsRouter.mjs";
+import messagesRouter from "./routes/messagesRouter.mjs";
+import userRoutes from "./routes/userRoutes.mjs";
 import { Server } from "socket.io";
 
 const app = express();
-const server = createServer(app)
+const server = createServer(app);
 
 // Enabling cors
 app.use(
   cors({
     origin: process.env.FRONTEND_URL,
     credentials: true,
-  })
+  }),
 );
 
 // Parsing data
@@ -40,24 +40,28 @@ connectDB()
 app.use(authRouter);
 app.use(chatsRouter);
 app.use(messagesRouter);
-app.use(userRoutes)
+app.use(userRoutes);
+
+// pings
+app.get("/health", (_req, res) => {
+  return res.sendStatus(200);
+});
 
 // socket integration
 export const io = new Server(server, {
   cors: {
     origin: process.env.FRONTEND_URL,
-    credentials: true
-  }
-}) 
+    credentials: true,
+  },
+});
 
 export const socketMap = new Map();
 
-io.on("connection", (socket)=>{
-  
-  socket.on("register", (userId)=>{
-    socketMap.set(userId, socket.id)
-  })
-})
+io.on("connection", (socket) => {
+  socket.on("register", (userId) => {
+    socketMap.set(userId, socket.id);
+  });
+});
 
 // Starting server
 server.listen(process.env.PORT, () => {
